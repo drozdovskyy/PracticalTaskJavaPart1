699c74403238fd9babdcd487543eec12cdb1732f
drozdovskyy <drozdovskyj@gmail.com>
2023-11-17 13:09:23 +0100

    Scanner upgraded

--- src/Task1CalculationOfTheFinalProductPriceIncludingVAT.java
+++ src/Task1CalculationOfTheFinalProductPriceIncludingVAT.java
@@ -4,22 +4,31 @@ import java.util.Scanner;
 class Task1CalculationOfTheFinalProductPriceIncludingVAT {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String priceRequirement = "Invalid data. Please enter valid data (positive number with no more than two decimals.";
         double priceExcludingVAT;
         double priceIncludingVAT;
         boolean isPositiveAndHasTwoDecimals;
-        String requirement = "";
+        String requirement = "Invalid data. Please enter valid data (positive number with no more than two decimals.";
 
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter price of a product excluding VAT:");
+
         do {
-            System.out.println("Enter price of a product excluding VAT" + requirement + ": ");
-            priceExcludingVAT = scanner.nextDouble();
+            while (!scanner.hasNextDouble()){
+                System.out.println(priceRequirement);
+                scanner.next();
+            }
 
+            priceExcludingVAT = scanner.nextDouble();
             isPositiveAndHasTwoDecimals = (BigDecimal.valueOf(priceExcludingVAT).scale() <= 2) && (priceExcludingVAT > 0);
-            requirement = " (positive, no more than two decimals)";
+
+            if (!isPositiveAndHasTwoDecimals) {
+                System.out.println(priceRequirement);
+            }
+
         } while (!isPositiveAndHasTwoDecimals);
 
         priceIncludingVAT = priceExcludingVAT * 1.2;
-
         System.out.printf("Price excluding VAT: %.2f, Price including VAT: %.2f",priceExcludingVAT, priceIncludingVAT);
 
         scanner.close();
